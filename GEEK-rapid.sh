8ef90e5864e00a7c94290b0c705de594d30ff1e8
AnderSon <son@lincolnix.net>
2015-11-10 19:28:41 -0600

    initial commit

---
+++ GEEK-rapid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PURPOSE:
+#The purpose of GEEK-rapid.sh is to repeat the action of running GEEK.sh
+
+#TESTS:
+#test comment for symlink
+
+while true
+do
+    $HOME/bin/GEEK.sh $1
+    sleep 0.25
+done
+
+ruby ~/bin/Ruby/GEEKwkDirCleaner.rb
+
